56f25c78d667f83b37d07e9f98db4bc951b0bd92
Nachiket Athavale <Nachiket.Athavale@cognizant.com>
2019-12-07 17:59:51 +0530

    Updated Test Cases for Task and Project Controllers

--- ProjectManager.Tests/ProjectControllerTest.cs
+++ ProjectManager.Tests/ProjectControllerTest.cs
@@ -12,75 +12,7 @@ namespace ProjectManager.Test
 
     [TestClass]
     public class ProjectControllerTest
-    {
-        [TestMethod]
-        public void TestGetProjects_Success()
-        {
-            var context = new MockProjectManagerEntities();
-            var projects = new TestDbSet<DAC.Project>();
-            projects.Add(new DAC.Project()
-            {
-                Project_ID = 1234,
-                Project_Name = "MyProject",
-                Start_Date = DateTime.Now,
-                End_Date = DateTime.Now.AddDays(5),
-                Priority = 3
-            });
-            projects.Add(new DAC.Project()
-            {
-                Project_ID = 12345,
-                Project_Name = "MyProject",
-                Start_Date = DateTime.Now,
-                End_Date = DateTime.Now.AddDays(5),
-                Priority = 3
-            });
-            context.Projects = projects;
-
-            var controller = new ProjectController(new BC.ProjectBC(context));
-            var result = controller.RetrieveProjects() as JSendResponse;
-
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result.Data, typeof(List<Project>));
-            Assert.AreEqual((result.Data as List<Project>).Count, 2);
-        }
-
-        [TestMethod]
-        public void TestInsertProjects_Success()
-        {
-            var context = new MockProjectManagerEntities();
-            var users = new TestDbSet<DAC.User>();
-            users.Add(new DAC.User()
-            {
-                Employee_ID = "401628",
-                First_Name = "Amantrita",
-                Last_Name = "Ghosh",
-                User_ID = 123,
-                Task_ID = 123
-            });
-            context.Users = users;
-            var testProject = new Models.Project()
-            {
-                ProjectId = 12345,
-                ProjectName = "MyProject",
-                ProjectStartDate = DateTime.Now,
-                ProjectEndDate = DateTime.Now.AddDays(5),
-                Priority = 3,
-                NoOfCompletedTasks = 3,
-                NoOfTasks = 5,
-                User = new User()
-                {
-                    FirstName = "Amantrita",
-                    LastName = "Ghosh",
-                    EmployeeId = "123456",
-                    UserId = 123
-                }
-            };
-            var controller = new ProjectController(new BC.ProjectBC(context));
-            var result = controller.InsertProjectDetails(testProject) as JSendResponse;
-
-            Assert.IsNotNull(result);
-            Assert.IsNotNull((context.Users.Local[0]).Project_ID);
-        }
+    {        
 
         [TestMethod]
         public void TestUpdateProjects_Success()
