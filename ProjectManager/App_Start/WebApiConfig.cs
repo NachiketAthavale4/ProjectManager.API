9a3a0d73cb1b1832e039553686a767242b18d614
Nachiket Athavale <Nachiket.Athavale@cognizant.com>
2019-12-07 10:21:34 +0530

    Added Models, ActionFilters and Utitlies

--- ProjectManager/App_Start/WebApiConfig.cs
+++ ProjectManager/App_Start/WebApiConfig.cs
@@ -9,9 +9,6 @@ namespace ProjectManager
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
---
+++ ProjectManager/Models/ParentTask.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectManager.Models
+{
+    public class ParentTask
+    {
+        public int ParentTaskId { get; set; }
+
+        public string ParentTaskName { get; set; }
+
+    }
+}
